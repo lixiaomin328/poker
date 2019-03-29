07693061b1aebae797ecd71e0058d98bdd459243
Virginia Fedrigo <vafedrigo@gmail.com>
2019-03-29 13:17:02 -0700

    fix pupil

--- data processing scripts/matchGazeFile.m
+++ data processing scripts/matchGazeFile.m
@@ -5,12 +5,17 @@ allEntries = findConditionalTrial();
 pupilOfInterest = [];
 for i = 1:length(allEntries)
     subId = allEntries(i).subjectId;
-filesContain = dir(sprintf('%s%02d_._*.mat',gazeFolder,subId));
-if isempty(filesContain)
-    continue;
+    filesContain = dir(sprintf('%s%02d_._*.mat',gazeFolder,subId));
+    if isempty(filesContain)
+        continue;
+    end
+    load([gazeFolder,filesContain.name])
+    %to add all the pupil entries for each trial
+    for j = 1:length(allEntries(i).trialNumbers)
+        what = (allEntries(i).trialNumbers(j));
+        where = find(data_et.trialIndex == what);
+        pupilOfInterest = [pupilOfInterest;data_et.pupilSize(where)];
+    end
 end
-load([gazeFolder,filesContain.name])
-pupilOfInterest = [pupilOfInterest;data_et.pupilSize(allEntries(i).trialNumbers)];
-end
-pupilInAllCards{j} = pupilOfInterest;
+%pupilInAllCards{j} = pupilOfInterest;
 %end
