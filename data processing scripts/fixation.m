549c39de0dd8f73b57cdfce6e516fabcfd58eee0
hottup <lixiaominxiaomin@gmail.com>
2019-04-11 17:29:41 -0700

    fixation analysis

--- data processing scripts/fixation.m
+++ data processing scripts/fixation.m
@@ -2,6 +2,7 @@
 %%With different card
 gazeFolder = 'processedGazeDataMat/';
 pupilInAllCards= cell(8,1);
+screenSize =[1024,1280];
 for k = 2:8
     allEntries = findConditionalTrial(k);
     pupilOfInterestAllPeople = [];
@@ -16,7 +17,7 @@ for k = 2:8
         end
         load([gazeFolder,filesContain.name])
         %to add all the pupil entries for each trial
-        for j = 2%1:length(allEntries(i).trialNumbers)
+        for j = 1:length(allEntries(i).trialNumbers)
             what = (allEntries(i).trialNumbers(j));
             where = find(data_et.trialIndex == what& data_et.eventType==1);
             if isempty(where)
@@ -31,4 +32,21 @@ for k = 2:8
         pupilOfInterestAllPeople = [pupilOfInterestAllPeople;fixationOfInterest];
     end
     pupilInAllCards{k} = pupilOfInterestAllPeople;
+end
+addpath('/Users/lixiaomin/Documents/GitHub/poker/fixation-visualization');
+img = imread('../poker screenshots/screen1.PNG');
+img = imresize(img,screenSize);
+
+%%
+figure()
+numberOfTomFixation = [];
+for j = 2:8
+fixationDecenter = pupilInAllCards{j}(:,1:2);
+fixationDecenter = fixationDecenter(fixationDecenter(:,1)>900&fixationDecenter(:,2)<400,:);
+%fixationDecenter = fixationDecenter(fixationDecenter(:,2)<300|fixationDecenter(:,2)>600,:);
+numberOfTomFixation = [numberOfTomFixation;length(fixationDecenter)];
+subplot(2,4,j)
+plotFixationHeatmap(img, fixationDecenter);
+title(['card',' ',num2str(j)]);
+hold on
 end
