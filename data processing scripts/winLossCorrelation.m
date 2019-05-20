92f7aec10bc38957937c5d78b806c5466b7aec43
Virginia Fedrigo <vafedrigo@gmail.com>
2019-05-20 15:29:05 -0700

    regression update

--- data processing scripts/winLossCorrelation.m
+++ data processing scripts/winLossCorrelation.m
@@ -10,11 +10,21 @@ for i = 1:length(dataFiles)
     p1Card = dataStructure.P1card;
     p2Card = dataStructure.P2card;
     [~,~, indicatorWin]= earningsCalc(p1Move, p2Move, p1Card, p2Card);
-    %indicatorWin is -1 when P2 wins, +1 when P1 wins
+    %indicatorWin is 2 when P2 wins, +1 when P1 wins
+    %modify indicatorWin
+    whereNeg = find(indicatorWin == -1);
+    indicatorWin(whereNeg) = 2;
+    
     shiftedP1actions = [(p1Move(1:end-1))', 0]';
     whereCheck = find(shiftedP1actions == 0);
-    shiftedP1actions(whereCheck) = -1; %so all checks are now -1
+    shiftedP1actions(whereCheck) = 2; %so all checks are now 2
+    whereTO = find(shiftedP1actions == -1);
+    if ~isempty(whereTO) %remove TO
+        shiftedP1actions(whereTO) = [];
+        indicatorWin(whereTO) = [];
+    end
     
     savedCorr(1, i) = corr(shiftedP1actions, indicatorWin);
-    lm = fitlm(indicatorWin, shiftedP1actions)
+    statsname = char(compose('stats%d', i));
+    [B,dev,statsname] = mnrfit(indicatorWin, shiftedP1actions)
 end
