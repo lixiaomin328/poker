cfec45491300eb254f308fb8a8cd79f951b25d87
Xiaomin Li <xiaominli@xiaomins-mbp.lan>
2019-06-05 13:35:12 -0700

    RT model

--- model_fitting/llNormal.m
+++ model_fitting/llNormal.m
@@ -1,5 +1,5 @@
 function loglk = llNormal(data,mu,sigma)
 loglk = 0;
 for i = 1:length(data)
-    loglk = loglk + log(normcdf(data(i),mu,sigma));
+    loglk = loglk + log(normpdf(data(i),mu,sigma));
 end
